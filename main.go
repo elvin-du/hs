460a1fb6eb0c83ccfd2424f5b524b5ac41dd3a37
nick Du <nick@yun.io>
2015-01-06 15:06:53 +0800

    redis pool ok

--- main.go
+++ main.go
@@ -3,24 +3,61 @@ package main
 import (
 	"hs/database"
 	"log"
+	"math/rand"
+	"strconv"
 	"time"
 )
 
 func main() {
 	database.Start()
-	go Query()
-	go Query()
-	go Query()
-	go Query()
+	//go Query()
+	//go Query()
+	//go Query()
+	//go Query()
+	go RedisGet()
+	go RedisGet()
+	go RedisGet()
+	go RedisGet()
+	go RedisGet()
 
 	time.Sleep(time.Second * 50)
-	go Query()
-	go Query()
-	go Query()
-	go Query()
+	go RedisGet()
+	go RedisGet()
+	go RedisGet()
+	go RedisGet()
+	go RedisGet()
+	//go Query()
+	//go Query()
+	//go Query()
+	//go Query()
 
 	ch := make(chan byte)
 	<-ch
+}
+
+func RedisGet() {
+	redis, err := database.GetMRedis()
+	if nil != err {
+		log.Println(err)
+		return
+	}
+
+	client := redis.Client()
+
+	val := strconv.Itoa(rand.Int())
+	err = client.Set("zoe", val).Err()
+	if nil != err {
+		log.Println(err)
+		return
+	}
+	log.Println("set zoe value:", val)
+
+	value, err := client.Get("zoe").Result()
+	if nil != err {
+		log.Println(err)
+		return
+	}
+	log.Println(value)
 }
 
 func Query() {
